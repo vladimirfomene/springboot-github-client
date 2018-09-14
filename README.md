f93b5891148af49a1e98fc80c801cb574aa7c6b2
Vladimir Fomene <vladimirfomene@gmail.com>
2018-09-14 05:24:19 +0100

    Add README file

---
+++ README.md
@@ -0,0 +1,1 @@
+# springboot-github-client
---
+++ src/main/java/com/auth0/samples/springbootgithubclient/SpringbootGithubClientApplication.java
@@ -0,0 +1,12 @@
+package com.auth0.samples.springbootgithubclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGithubClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGithubClientApplication.class, args);
+	}
+}
